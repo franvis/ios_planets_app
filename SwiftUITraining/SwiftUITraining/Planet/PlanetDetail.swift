2f8f34d7e95753557a9a0c0848a9a3b2be7594f4
Emiliano Alvarez <emiliano.alvarez@coolblue.nl>
2020-06-06 21:48:24 +0200

    Add Planet Detail

---
+++ SwiftUITraining/SwiftUITraining/Planet/PlanetDetail.swift
@@ -0,0 +1,41 @@
+//
+//  PlanetDetail.swift
+//  SwiftUITraining
+//
+//  Created by Emiliano Alvarez on 06/06/2020.
+//  Copyright © 2020 Coolblue. All rights reserved.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct PlanetDetail: View {
+
+    let planet: Planet
+
+    var body: some View {
+        ZStack {
+            Image("Sky")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                AnimatedImage(url: planet.imageUrl)
+                    .resizable()
+                    .frame(width: 250, height: 250)
+                Text(planet.name)
+                    .font(Font.custom("Helvetica", size: 48))
+                    .foregroundColor(.white)
+                Text(planet.shortDescription)
+                    .font(Font.custom("Helvetica", size: 18))
+                    .foregroundColor(.white)
+                Spacer()
+            }
+        }
+    }
+}
+
+struct PlanetDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        PlanetDetail(planet: PlanetMockData.planets[0])
+    }
+}
